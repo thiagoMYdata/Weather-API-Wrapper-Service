f1a07ccd652b3228516cb2e12b83a23228ba9808
Thiago Blanco <thiago.dataislife@gmail.com>
2026-07-21 16:53:57 -0300

    integrate DRF with third-party API

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import os
+from dotenv import load_dotenv
+import requests
 
-def firstSteps(requests):
-    return HttpResponse("<h1>Hello World</h1>")
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+load_dotenv()
+
+
+@api_view(["GET"])
+def apiTest(request):
+    city = "madrid"
+    api_key = os.environ.get("API_KEY")
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/?key={api_key}"
+    r = requests.get(url)
+    if r.status_code == 200:
+        data = r.json()
+        today = data["days"][0]
+        todayJson = {
+            "city" : data["resolvedAddress"],
+            "temp" : today["temp"],
+            "temp_max" : today["tempmax"],
+            "temp_min" : today["tempmin"],
+            "climate_conditions" : today["conditions"],
+            "description" : today["description"]
+        }
+        return JsonResponse(todayJson)
+
+
+if __name__ == "__main__":
+    apiTest()
