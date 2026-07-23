836cc459c4c2c32a6553e1db8569a10e3f16ae01
Thiago Blanco <thiago.dataislife@gmail.com>
2026-07-23 16:26:23 -0300

    add low level caching

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -2,30 +2,36 @@ import os
 from dotenv import load_dotenv
 import requests
 
-from django.views.decorators.cache import cache_page
-from django.views.decorators.vary import vary_on_cookie 
+from django.core.cache import cache
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
 
 load_dotenv()
-@cache_page(15)
-@vary_on_cookie
+API_KEY = os.environ.get("API_KEY")
+
+
 @api_view(["GET"])
-def apiTest(request):
+def WeatherAPIWrapper(request):
     city = request.data["city"]
-    api_key = os.environ.get("API_KEY")
-    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/?key={api_key}"
-    r = requests.get(url)
-    if r.status_code == 200:
-        data = r.json()
-        today = data["days"][0]
-        todayJson = {
-            "city" : data["resolvedAddress"],
-            "temp" : today["temp"],
-            "temp_max" : today["tempmax"],
-            "temp_min" : today["tempmin"],
-            "climate_conditions" : today["conditions"],
-            "description" : today["description"]
-        }
-        return Response(todayJson)
+    if cache.get(city) != None: 
+        return Response(cache.get(city))
+    else:
+        url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/?key={API_KEY}"
+        r = requests.get(url)
+        if r.status_code == 200:
+            data = r.json()
+            today = data["days"][0]
+            todayJson = {
+                "city" : data["resolvedAddress"],
+                "temp" : today["temp"],
+                "temp_max" : today["tempmax"],
+                "temp_min" : today["tempmin"],
+                "climate_conditions" : today["conditions"],
+                "description" : today["description"]
+            }
+            cache.set(key=city, value=todayJson, timeout=10)
+            return Response(todayJson)
+        return Response({"message" : "api request error"}, status=status.HTTP_502_BAD_GATEWAY)
